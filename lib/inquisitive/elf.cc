3fd54cb119ae072e5af8d7677b6492d30ce8d1c6
Force Charlie <charlieio@outlook.com>
2018-12-25 21:50:31 +0800

    Add ElfArchitecture to get arch name

--- lib/inquisitive/elf.cc
+++ lib/inquisitive/elf.cc
@@ -16,6 +16,243 @@ struct StringEntry {
   int IndexInSection;
 };
 
+const wchar_t *OSABI(uint8_t osabi) {
+  switch (osabi) {
+  case ELFOSABI_SYSV:
+    return L"System V";
+  case ELFOSABI_HPUX:
+    return L"HP-UX";
+  case ELFOSABI_NETBSD:
+    return L"NetBSD";
+  case ELFOSABI_LINUX:
+    return L"Linux";
+  case 4: /// musl not defined
+    return L"GNU Hurd";
+  case ELFOSABI_SOLARIS:
+    return L"Solaris";
+  case ELFOSABI_AIX:
+    return L"AIX";
+  case ELFOSABI_IRIX:
+    return L"IRIX";
+  case ELFOSABI_FREEBSD:
+    return L"FreeBSD";
+  case ELFOSABI_TRU64:
+    return L"Tru64";
+  case ELFOSABI_MODESTO:
+    return L"Novell Modesto";
+  case ELFOSABI_OPENBSD:
+    return L"OpenBSD";
+  case 0x0D:
+    return L"OpenVMS";
+  case 0x0E:
+    return L"NonStop Kernel";
+  case 0x0F:
+    return L"AROS";
+  case 0x10:
+    return L"Fenix OS";
+  case 0x11:
+    return L"CloudABI";
+  case ELFOSABI_ARM:
+    return L"ARM";
+  default:
+    break;
+  }
+  return L"UNKNOWN";
+}
+struct elf_kv_t {
+  uint32_t index;
+  const wchar_t *value;
+};
+
+const wchar_t *ElfArchitecture(uint32_t e) {
+  const elf_kv_t kv[] = {
+      {EM_M32, L"M32"},
+      {EM_SPARC, L"SPARC"},
+      {EM_386, L"x86"},
+      {EM_68K, L"Motorola 68000"},
+      {EM_88K, L"Motorola 88000"},
+      {EM_860, L"860"},
+      {EM_MIPS, L"MIPS"},
+      {EM_S370, L"IBM System/370"},
+      {EM_MIPS_RS3_LE, L"MIPS RS3 LE"},
+      {EM_PARISC, L"PA-RISC"},
+      {EM_VPP500, L"VPP500"},
+      {EM_SPARC32PLUS, L"SPARC32PLUS"},
+      {EM_960, L"960"},
+      {EM_PPC, L"PPC"},
+      {EM_PPC64, L"PPC64"},
+      {EM_S390, L"IBM System/390"},
+      {EM_V800, L"V800"},
+      {EM_FR20, L"FR20"},
+      {EM_RH32, L"RH32"},
+      {EM_RCE, L"RCE"},
+      {EM_ARM, L"ARM"},
+      {EM_FAKE_ALPHA, L"EM_FAKE_ALPHA"},
+      {EM_SH, L"EM_SH"},
+      {EM_SPARCV9, L"SPARC V9"},
+      {EM_TRICORE, L"TRICORE"},
+      {EM_ARC, L"ARC"},
+      {EM_H8_300, L"H8 300"},
+      {EM_H8_300H, L"H8 300H"},
+      {EM_H8S, L"EM_H8S"},
+      {EM_H8_500, L"EM_H8_500"},
+      {EM_IA_64, L"IA64"},
+      {EM_MIPS_X, L"MIPS X"},
+      {EM_COLDFIRE, L"ColdFire"},
+      {EM_68HC12, L"68HC12"},
+      {EM_MMA, L"MMA"},
+      {EM_PCP, L"PCP"},
+      {EM_NCPU, L"NCPU"},
+      {EM_NDR1, L"NDR1"},
+      {EM_STARCORE, L"STARCORE"},
+      {EM_ME16, L"ME16"},
+      {EM_ST100, L"ST100"},
+      {EM_TINYJ, L"TINYJ"},
+      {EM_X86_64, L"x86-64"},
+      {EM_PDSP, L"PDSP"},
+      {EM_FX66, L"FX66"},
+      {EM_ST9PLUS, L"ST9Plus"},
+      {EM_ST7, L"ST7"},
+      {EM_68HC16, L"68HC16"},
+      {EM_68HC11, L"68HC11"},
+      {EM_68HC08, L"68HC08"},
+      {EM_68HC05, L"68HC05"},
+      {EM_SVX, L"SVX"},
+      {EM_ST19, L"ST19"},
+      {EM_VAX, L"VAX"},
+      {EM_CRIS, L"CRIS"},
+      {EM_JAVELIN, L"Javelin"},
+      {EM_FIREPATH, L"Firepath"},
+      {EM_ZSP, L"ZSP"},
+      {EM_MMIX, L"MMIX"},
+      {EM_HUANY, L"Huany"},
+      {EM_PRISM, L"Prism"},
+      {EM_AVR, L"AVR"},
+      {EM_FR30, L"FR30"},
+      {EM_D10V, L"D10V"},
+      {EM_D30V, L"D30V"},
+      {EM_V850, L"V850"},
+      {EM_M32R, L"M32R"},
+      {EM_MN10300, L"MN10300"},
+      {EM_MN10200, L"MN10200"},
+      {EM_PJ, L"PJ"},
+      {EM_OR1K, L"OR1K"},
+      {EM_OPENRISC, L"OpenRISC"},
+      {EM_ARC_A5, L"ARC A5"},
+      {EM_ARC_COMPACT, L"ARC Compact"},
+      {EM_XTENSA, L"Xtensa"},
+      {EM_VIDEOCORE, L"Videocore"},
+      {EM_TMM_GPP, L"TMM GPP"},
+      {EM_NS32K, L"NS32K"},
+      {EM_TPC, L"TPC"},
+      {EM_SNP1K, L"SNP1K"},
+      {EM_ST200, L"ST200"},
+      {EM_IP2K, L"IP2K"},
+      {EM_MAX, L"MAX"},
+      {EM_CR, L"CR"},
+      {EM_F2MC16, L"F2MC16"},
+      {EM_MSP430, L"MSP430"},
+      {EM_BLACKFIN, L"Blackfin"},
+      {EM_SE_C33, L"SE C33"},
+      {EM_SEP, L"SEP"},
+      {EM_ARCA, L"ARCA"},
+      {EM_UNICORE, L"Unicore"},
+      {EM_EXCESS, L"Excess"},
+      {EM_DXP, L"DXP"},
+      {EM_ALTERA_NIOS2, L"Altera NIOS2"},
+      {EM_CRX, L"CRX"},
+      {EM_XGATE, L"XGate"},
+      {EM_C166, L"C166"},
+      {EM_M16C, L"M16C"},
+      {EM_DSPIC30F, L"DSPIC30F"},
+      {EM_CE, L"CE"},
+      {EM_M32C, L"M32C"},
+      {EM_TSK3000, L"TSK3000"},
+      {EM_RS08, L"RS08"},
+      {EM_SHARC, L"SHARC"},
+      {EM_ECOG2, L"ECOG2"},
+      {EM_SCORE7, L"SCORE7"},
+      {EM_DSP24, L"DSP24"},
+      {EM_VIDEOCORE3, L"Videocore3"},
+      {EM_LATTICEMICO32, L"Latticemico32"},
+      {EM_SE_C17, L"SE C17"},
+      {EM_TI_C6000, L"TI C6000"},
+      {EM_TI_C2000, L"TI C2000"},
+      {EM_TI_C5500, L"TI C5500"},
+      {EM_TI_ARP32, L"TI ARP32"},
+      {EM_TI_PRU, L"TI PRU"},
+      {EM_MMDSP_PLUS, L"MMDSP Plus"},
+      {EM_CYPRESS_M8C, L"CYRPESS M8C"},
+      {EM_R32C, L"R32C"},
+      {EM_TRIMEDIA, L"Trimedia"},
+      {EM_QDSP6, L"QDSP6"},
+      {EM_8051, L"8051"},
+      {EM_STXP7X, L"STXP7X"},
+      {EM_NDS32, L"NDS32"},
+      {EM_ECOG1X, L"ECOG1X"},
+      {EM_MAXQ30, L"MAXQ30"},
+      {EM_XIMO16, L"XIMO16"},
+      {EM_MANIK, L"Manik"},
+      {EM_CRAYNV2, L"Craynv2"},
+      {EM_RX, L"RX"},
+      {EM_METAG, L"Metag"},
+      {EM_MCST_ELBRUS, L"MCST ELBRUS"},
+      {EM_ECOG16, L"ECOG16"},
+      {EM_CR16, L"CR16"},
+      {EM_ETPU, L"ETPU"},
+      {EM_SLE9X, L"SLE9X"},
+      {EM_L10M, L"L10M"},
+      {EM_K10M, L"K10M"},
+      {EM_AARCH64, L"AArch64"},
+      {EM_AVR32, L"AVR32"},
+      {EM_STM8, L"STM8"},
+      {EM_TILE64, L"Tile64"},
+      {EM_TILEPRO, L"TilePro"},
+      {EM_MICROBLAZE, L"Microblaze"},
+      {EM_CUDA, L"CUDA"},
+      {EM_TILEGX, L"Tilegx"},
+      {EM_CLOUDSHIELD, L"CloudShield"},
+      {EM_COREA_1ST, L"Corea 1st"},
+      {EM_COREA_2ND, L"Corea 2nd"},
+      {EM_ARC_COMPACT2, L"ARC Compact2"},
+      {EM_OPEN8, L"Open8"},
+      {EM_RL78, L"Rl78"},
+      {EM_VIDEOCORE5, L"Videocore5"},
+      {EM_78KOR, L"78KOR"},
+      {EM_56800EX, L"56800EX"},
+      {EM_BA1, L"BA1"},
+      {EM_BA2, L"BA2"},
+      {EM_XCORE, L"XCore"},
+      {EM_MCHP_PIC, L"Mchp Pic"},
+      {EM_KM32, L"KM32"},
+      {EM_KMX32, L"KMX32"},
+      {EM_EMX16, L"EMX16"},
+      {EM_EMX8, L"EMX8"},
+      {EM_KVARC, L"KVARC"},
+      {EM_CDP, L"CDP"},
+      {EM_COGE, L"Coge"},
+      {EM_COOL, L"COOL"},
+      {EM_NORC, L"Norc"},
+      {EM_CSR_KALIMBA, L"Kalimba"},
+      {EM_Z80, L"z80"},
+      {EM_VISIUM, L"Visium"},
+      {EM_FT32, L"FT32"},
+      {EM_MOXIE, L"Moxie"},
+      {EM_AMDGPU, L"AMDGPU"},
+      {EM_RISCV, L"RISC-V"},
+      {EM_BPF, L"BPF"},
+      {EM_NUM, L"NUM"},
+      {EM_ALPHA, L"EM_ALPHA"}
+      ///
+  };
+  for (const auto &k : kv) {
+    if (k.index == e) {
+      return k.value;
+    }
+  }
+  return L"No specific instruction set";
+}
+
 bool elfimagelookup32(memview mv, elf_minutiae_t &em) {
   auto h = mv.cast<Elf32_Ehdr>(0);
   if (h == nullptr) {
@@ -51,5 +288,16 @@ bool elfimagelookup(memview mv, elf_minutiae_t &em) {
     return false;
   }
   return true;
+}
+
+std::optional<elf_minutiae_t> inquisitive_elf(std::wstring_view sv,
+                                              std::error_code &ec) {
+  planck::mapview mv;
+  if (!mv.mapfile(sv, sizeof(Elf32_Ehdr))) {
+    return std::nullopt;
+  }
+  auto eh = mv.cast<Elf32_Ehdr>(0);
+
+  return std::nullopt;
 }
 } // namespace inquisitive
