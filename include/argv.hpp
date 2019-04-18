f725b66c94874818034f5df33dd02804e488d184
Force Charlie <charlieio@outlook.com>
2019-04-18 21:37:28 +0800

    Add argv.hpp

--- include/argv.hpp
+++ include/argv.hpp
@@ -1,130 +1,200 @@
-#pragma once
-#include <cstring>
+///////
+#ifndef PLANCK_ARGV_HPP
+#define PLANCK_ARGV_HPP
+#include "strcat.hpp"
 #include <functional>
+#include <string>
 #include <string_view>
 #include <vector>
-#include "charconv.hpp"
-#include "errorcode.hpp"
 
 namespace planck {
+
+enum ParseError {
+  SkipParse = -1,
+  None = 0,
+  ErrorNormal = 1 //
+};
+struct error_code {
+  std::wstring message;
+  int ec{0};
+  explicit operator bool() const noexcept { return ec != None; }
+  void Assign(std::wstring_view msg, int val = ErrorNormal) {
+    ec = val;
+    message.assign(msg);
+  }
+  template <typename... Args> void Assign(int val, Args... args) {
+    ec = val;
+    std::initializer_list<std::wstring_view> as = {
+        static_cast<const AlphaNum &>(args).Piece()...};
+    message = internal::CatPieces(as);
+  }
+};
+enum HasArgs {
+  required_argument, /// -i 11 or -i=xx
+  no_argument,
+  optional_argument /// -s --long --long=xx
+};
+struct option {
+  std::wstring_view name;
+  HasArgs has_args;
+  int val;
+};
+using invoke_t = std::function<bool(int, const wchar_t *, const wchar_t *)>;
 class ParseArgv {
 public:
-  enum HasArgs {
-    required_argument, /// -i 11 or -i=xx
-    no_argument,
-    optional_argument /// -s --long --long=xx
-  };
-  struct option {
-    const wchar_t *name;
-    HasArgs has_args;
-    int val;
-  };
-  using ArgvInvoke = std::function<bool(int, const wchar_t *, const wchar_t *)>;
+  using StringArray = std::vector<std::wstring_view>;
   ParseArgv(int argc, wchar_t *const *argv) : argc_(argc), argv_(argv) {}
   ParseArgv(const ParseArgv &) = delete;
   ParseArgv &operator=(const ParseArgv &) = delete;
-
-  base::error_code ParseArgument(const std::vector<option> &opts,
-                                 const ArgvInvoke &callback) {
-    if (argc_ == 0 || argv_ == nullptr) {
-      return base::make_error_code(L"invalid argument input");
-    };
-    index = 1;
-    for (; index < argc_; index++) {
-      std::wstring_view arg = argv_[index];
-      if (arg[0] != '-') {
-        uargs.push_back(arg);
-        continue;
-      }
-      auto ec = ParseInternal(arg, opts, callback);
-      if (ec) {
-        return ec;
-      }
-    }
-    return base::error_code();
+  ParseArgv &Add(std::wstring_view name, HasArgs a, int val) {
+    options_.push_back({name, a, val});
+    return *this;
   }
-  const std::vector<std::wstring_view> &UnresolvedArgs() const { return uargs; }
+  bool Execute(const invoke_t &v, error_code &ec);
+  const StringArray &UnresolvedArgs() const { return uargs; }
 
 private:
   int argc_;
   wchar_t *const *argv_;
-  std::vector<std::wstring_view> uargs;
   int index{0};
-  base::error_code ParseInternal(std::wstring_view arg,
-                                 const std::vector<option> &opts,
-                                 const ArgvInvoke &callback) {
-    /*
-    -x ; -x value -Xvalue
-    --xy;--xy=value;--xy value
-    */
-    if (arg.size() < 2) {
-      return base::strcat_error_code(L"Invaild argument '", arg, L"'");
-    }
-    int ch = -1;
-    HasArgs ha = optional_argument;
-    const wchar_t *optarg = nullptr;
-
-    if (arg[1] == '-') {
-      /// parse long
-      /// --name value; --name=value
-      std::wstring_view name;
-      auto pos = arg.find('=');
-      if (pos != std::wstring_view::npos) {
-        if (pos + 1 >= arg.size()) {
-          return base::strcat_error_code(L"Incorrect argument: ", arg);
-        }
-        name = arg.substr(2, pos - 2);
-        optarg = arg.data() + pos + 1;
-      } else {
-        name = arg.substr(2);
-      }
-      for (auto &o : opts) {
-        if (name.compare(o.name) == 0) {
-          ch = o.val;
-          ha = o.has_args;
-          break;
-        }
-      }
-    } else {
-      /// parse short
-      /// -x=xxx
-      if (arg.size() == 3 && arg[2] == '=') {
-        return base::strcat_error_code(L"Incorrect argument: ", arg);
-      }
-      if (arg.size() > 3) {
-        if (arg[2] == '=') {
-          optarg = arg.data() + 3;
-        } else {
-          optarg = arg.data() + 2;
-        }
-      }
-      for (auto &o : opts) {
-        if (o.val == arg[1]) {
-          ch = o.val;
-          ha = o.has_args;
-          break;
-        }
-      }
-    }
-    if (ch == -1) {
-      return base::strcat_error_code(L"Unregistered option: ", arg);
-    }
-
-    if (optarg != nullptr && ha == no_argument) {
-      return base::strcat_error_code(L"Unacceptable input: ", arg);
-    }
-    if (optarg == nullptr && ha == required_argument) {
-      if (index + 1 >= argc_) {
-        return base::strcat_error_code(L"Argument missing value: ", arg);
-      }
-      optarg = argv_[index + 1];
-      index++;
-    }
-    if (callback(ch, optarg, arg.data())) {
-      return base::error_code();
-    }
-    return base::make_error_code(L"skipped");
-  }
+  StringArray uargs;
+  std::vector<option> options_;
+  bool parse_internal(std::wstring_view a, const invoke_t &v, error_code &ec);
+  bool parse_internal_long(std::wstring_view a, const invoke_t &v,
+                           error_code &ec);
+  bool parse_internal_short(std::wstring_view a, const invoke_t &v,
+                            error_code &ec);
 };
 
+// ---------> parse internal
+inline bool ParseArgv::Execute(const invoke_t &v, error_code &ec) {
+  if (argc_ == 0 || argv_ == nullptr) {
+    ec.Assign(L"the command line array is empty.");
+    return false;
+  }
+  index = 1;
+  for (; index < argc_; index++) {
+    std::wstring_view a = argv_[index];
+    if (a.empty() || a.front() != '-') {
+      uargs.push_back(a);
+      continue;
+    }
+    // parse ---
+    if (!parse_internal(a, v, ec)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+inline bool ParseArgv::parse_internal_short(std::wstring_view a,
+                                            const invoke_t &v, error_code &ec) {
+  int ch = -1;
+  HasArgs ha = optional_argument;
+  const wchar_t *oa = nullptr;
+  // -x=XXX
+  // -xXXX
+  // -x XXX
+  // -x; BOOL
+  if (a[0] == '=') {
+    ec.Assign(1, L"unexpected argument '-", a, L"'"); // -=*
+    return false;
+  }
+  auto c = a[0];
+  for (const auto &o : options_) {
+    if (o.val == c) {
+      ch = o.val;
+      ha = o.has_args;
+      break;
+    }
+  }
+  if (ch == -1) {
+    ec.Assign(1, L"unregistered option '-", a, L"'");
+    return false;
+  }
+  // a.size()==1 'L' short value
+  if (a.size() >= 2) {
+    oa = (a[1] == L'=') ? (a.data() + 2) : (a.data() + 1);
+  }
+  if (oa != nullptr && ha == no_argument) {
+    ec.Assign(1, L"option '-", a.substr(0, 1), L"' unexpected parameter: ", oa);
+    return false;
+  }
+  if (oa == nullptr && ha == required_argument) {
+    if (index + 1 >= argc_) {
+      ec.Assign(1, L"option '-", a, L"' missing parameter");
+      return false;
+    }
+    oa = argv_[index + 1];
+    index++;
+  }
+  if (!v(ch, oa, a.data())) {
+    ec.Assign(SkipParse, L"skip parse");
+    return false;
+  }
+  return true;
+}
+
+// Parse long option
+inline bool ParseArgv::parse_internal_long(std::wstring_view a,
+                                           const invoke_t &v, error_code &ec) {
+  // --xxx=XXX
+  // --xxx XXX
+  // --xxx; bool
+  int ch = -1;
+  HasArgs ha = optional_argument;
+  const wchar_t *oa = nullptr;
+  auto pos = a.find(L'=');
+  if (pos != std::string_view::npos) {
+    if (pos + 1 >= a.size()) {
+      ec.Assign(1, L"unexpected argument '--", a, L"'");
+      return false;
+    }
+    oa = a.data() + pos + 1;
+    a.remove_suffix(a.size() - pos);
+  }
+  for (const auto &o : options_) {
+    if (o.name == a) {
+      ch = o.val;
+      ha = o.has_args;
+      break;
+    }
+  }
+  if (ch == -1) {
+    ec.Assign(1, L"unregistered option '--", a, L"'");
+    return false;
+  }
+  if (oa != nullptr && ha == no_argument) {
+    ec.Assign(1, L"option '--", a, L"' unexpected parameter: ", oa);
+    return false;
+  }
+  if (oa == nullptr && ha == required_argument) {
+    if (index + 1 >= argc_) {
+      ec.Assign(1, L"option '--", a, L"' missing parameter");
+      return false;
+    }
+    oa = argv_[index + 1];
+    index++;
+  }
+  if (!v(ch, oa, a.data())) {
+    ec.Assign(SkipParse, L"skip parse");
+    return false;
+  }
+  return true;
+}
+
+inline bool ParseArgv::parse_internal(std::wstring_view a, const invoke_t &v,
+                                      error_code &ec) {
+  if (a.size() == 1) {
+    ec.Assign(L"unexpected argument '-'");
+    return false;
+  }
+  if (a[1] == '-') {
+    return parse_internal_long(a.substr(2), v, ec);
+  }
+  return parse_internal_short(a.substr(1), v, ec);
+}
+
 } // namespace planck
+
+#endif
